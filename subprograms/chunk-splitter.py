899bc30c5cd5b7262b406819408f160571fffedd
Drewik <yura.skleroznik@gmail.com>
2022-12-04 15:33:46 +0500

    refactor chunk-splitter

--- subprograms/chunk-splitter.py
+++ subprograms/chunk-splitter.py
@@ -6,8 +6,8 @@ def csv_splitter(file_name, save_path):
     print(data[0])
     field_names = data[0]
     years = set()
-
-    years.add(data[1][-1][0:4])
+    curr_year = data[1][-1][0:4]
+    years.add(curr_year)
     lastindex = 1
     for i, row in enumerate(data):
         if len(row) != len(field_names):
@@ -15,19 +15,17 @@ def csv_splitter(file_name, save_path):
         if i == 0:
             continue
         year = row[-1][0:4]
-
         if (year not in years):
-            current_year = str(int(year) - 1)
-            newfile = open(save_path + current_year + ".csv", "w", encoding="utf-8-sig", newline='')
+            newfile = open(save_path + curr_year + ".csv", "w", encoding="utf-8-sig", newline='')
             writer = csv.writer(newfile)
             writer.writerow(field_names)
             writer.writerows(data[lastindex:i])
-            lastindex = i+1
+            lastindex = i
             years.add(year)
-        if row == data[-1]:
-            newfile = open(save_path + str(int(year)) + ".csv", "w", encoding="utf-8-sig", newline='')
-            writer = csv.writer(newfile)
-            writer.writerow(field_names)
-            writer.writerow(data[lastindex:i])
+            curr_year = year
+    newfile = open(save_path + curr_year + ".csv", "w", encoding="utf-8-sig", newline='')
+    writer = csv.writer(newfile)
+    writer.writerow(field_names)
+    writer.writerows(data[lastindex:len(data)])
 
 csv_splitter("vacancies_by_year.csv", "./chunks/")
